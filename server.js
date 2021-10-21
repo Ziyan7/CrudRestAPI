b8dbd86b68a3c93956c2e91617378447dda5c99e
Your Name <you@example.com>
2021-10-21 14:57:17 +0530

    Added : Swagger UI and Bcrypt

--- server.js
+++ server.js
@@ -9,10 +9,14 @@ const connect = require('./config/db.config.js');
 //import logger
 const logger = require('./logger/logger.js')
 
-// app.use(express.urlencoded({
-//     extended: false
-// }))
+app.use(express.urlencoded({
+    extended: false
+}))
 app.use(express.json())
+
+const swaggerUi = require('swagger-ui-express'),
+swaggerDocument = require('./swagger.json');
+
 // Require Notes routes
 require('./app/routes/note.routes.js')(app);
 require('./app/routes/user.routes.js')(app);
@@ -22,6 +26,14 @@ app.get('/', (req, res) => {
     res.json({"message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."});
 });
 
+
+app.use(
+    '/api-docs',
+    swaggerUi.serve, 
+    swaggerUi.setup(swaggerDocument)
+  );
+
+  
 // listen for requests
 app.listen(3000, () => {
     logger.info("Server is listening on port 3000");
